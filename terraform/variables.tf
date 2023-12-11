4fbd2102b0d0a80c1cd6a8d19b4d94d11648ea97
sm2424 <91906122+sm2424@users.noreply.github.com>
2023-12-11 19:38:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -89,7 +89,7 @@ variable "instance_type" {
 }
 variable "docker_image_url_django" {
   description = "Docker image to run in the ECS cluster"
-  default     = "600735812827.dkr.ecr.us-west-1.amazonaws.com/django-app:latest"
+  default     = "628664630832.dkr.ecr.us-west-1.amazonaws.com/django-app:latest"
 }
 variable "app_count" {
   description = "Number of Docker containers to run"
